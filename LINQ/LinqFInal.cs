89c8fa450bb601cf332d1fbb4d283597746d59c9
GovindSuccessive <govind.sharma@successive.tech>
2023-11-30 17:45:55 +0530

    LinqFInal HandOn

---
+++ LINQ/LinqFInal.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvancedTraining
+{
+    class LinqFInal
+    {
+        public static void MethodDeferredOrImmediate()
+        {
+            IList<Student> studentList = new List<Student>() {
+            new Student() { StudentID = 1, StudentName = "John", Age = 13 } ,
+            new Student() { StudentID = 2, StudentName = "Steve",Age = 15 } ,
+            new Student() { StudentID = 3, StudentName = "Bill", Age = 18 } ,
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 12 } ,
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 21 }
+           };
+
+            var teenAgerStudents = studentList.Select(s => s).ToList();
+            studentList.Add(new Student() { StudentID = 7, StudentName = "Ronit", Age = 28 });
+
+
+            foreach (Student teenStudent in teenAgerStudents)
+                Console.WriteLine("Student Name: {0}", teenStudent.StudentName);
+        }
+
+        public static void MethodElementAt()
+        {
+            IList<int> intList = new List<int>() { 10, 21, 30, 45, 50, 87 };
+            IList<string> strList = new List<string>() { "One", "Two", null, "Four", "Five" };
+
+            Console.WriteLine("1st Element in intList: {0}", intList.ElementAt(0));
+            Console.WriteLine("1st Element in strList: {0}", strList.ElementAt(0));
+
+            Console.WriteLine("2nd Element in intList: {0}", intList.ElementAt(1));
+            Console.WriteLine("2nd Element in strList: {0}", strList.ElementAt(1));
+
+            Console.WriteLine("3rd Element in intList: {0}", intList.ElementAtOrDefault(2));
+            Console.WriteLine("3rd Element in strList: {0}", strList.ElementAtOrDefault(2));
+
+            Console.WriteLine("10th Element in intList: {0} - default int value",
+                            intList.ElementAtOrDefault(9));
+            Console.WriteLine("10th Element in strList: {0} - default string value (null)",
+                             strList.ElementAtOrDefault(9));
+
+
+            Console.WriteLine("intList.ElementAt(9) throws an exception: Index out of range");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine(intList.ElementAt(9));
+        }
+
+
+        public static void MethodGroupBy()
+        {
+            IList<Student> studentList = new List<Student>() {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+            new Student() { StudentID = 5, StudentName = "Abram" , Age = 21 }
+    };
+
+            
+            var groupedResult = from s in studentList
+                                group s by s.Age;
+
+            //or
+
+            var resutl = studentList.Select(x => x).GroupBy(x => x.Age);
+
+            //iterate each group        
+            foreach (var ageGroup in resutl)
+            {
+                Console.WriteLine("Age Group: {0}", ageGroup.Key); //Each group has a key 
+
+                foreach (Student s in ageGroup) // Each group has inner collection
+                    Console.WriteLine("Student Name: {0}", s.StudentName);
+            }
+        }
+
+        public static void MethodLookUp()
+        {
+            IList<Student> studentList = new List<Student>() {
+        new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+        new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+        new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+        new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+        new Student() { StudentID = 5, StudentName = "Abram" , Age = 21 }
+    };
+
+            var lookupResult = studentList.ToLookup(s => s.Age);
+
+            foreach (var group in lookupResult)
+            {
+                Console.WriteLine("Age Group: {0}", group.Key);  //Each group has a key 
+
+                foreach (Student s in group)  //Each group has a inner collection  
+                    Console.WriteLine("Student Name: {0}", s.StudentName);
+            }
+        }
+
+    }
+
+}
